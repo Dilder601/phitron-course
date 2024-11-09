56d37336d1113d34ae9e4c8f838673ba9cdc6763
Dilder <dilder.hossain.feni@gmail.com>
2024-11-09 22:46:48 +0600

    Python Start

---
+++ Python/second.py
@@ -0,0 +1,12 @@
+a = 10;
+b = 30;
+name ="Phitron";
+_ ="naim";
+
+num1 =3;
+num2=2;
+_num = 12;
+
+c = a+b;
+
+print(c);
---
+++ Python/third.py
@@ -0,0 +1,5 @@
+userName = input("Enter Your Name:");
+userAge = input("Enter Your Age:");
+print("Hello! ", userName,"Your Age is: ",userAge);
+
+# input()=== always string formate a data nibe
