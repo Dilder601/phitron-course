fd4a2427da03870e489e4be2955af1006ddb7f98
Dilder <dilder.hossain.feni@gmail.com>
2024-11-20 22:54:10 +0600

    for loop

---
+++ Python/8.py
@@ -0,0 +1,14 @@
+# Day-4 Introduction To Looping
+
+# Types of looping 
+# 1. For loop
+# 2. While Loop
+# 3. Nested Loop
+
+
+n = 100;
+
+for i in range(n):
+    # print(str(i) +"-"+" - Hello World");
+    # print(f"{i}- - Hello World")
+    print("Nasa is hacking ",i,"%")
